6574a5102a3abfb32d6217144a613fb9b6ae88af
Colin <colinm@rspndr.io>
2024-11-04 16:57:06 -0500

    feat: build landing pages, routing

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
+// src/app/home/home-page.component.ts
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from '../app-header/app-header.component';
 
 @Component({
   selector: 'app-home-page',
+  standalone: true,
+  imports: [AppHeaderComponent],
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css'],
-  standalone: true
+  styleUrls: ['./home-page.component.css']
 })
-export class HomePageComponent {
-  userName = 'Bacchus'; // Example data, replace with actual user data
-}
+export class HomePageComponent {}
